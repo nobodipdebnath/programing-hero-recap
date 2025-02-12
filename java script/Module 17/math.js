c506a04a5006e89e4dbc14221ee4916367c9a6cb
Nobodip Debnath <nobodipdebnath45@.com>
2025-02-12 23:56:09 +0600

    add first file

---
+++ java script/Module 17/math.js
@@ -0,0 +1,13 @@
+// Math for Programming
+// 1 . Number +ve, -ve , zero
+// 2 . integer : 2, 88 [odd, even][3, 4]
+// 3 . float : 5.5, 6.5
+// 4 . Operation : +, -, *, /
+// 5 . More operation : %, [reminder]
+// 6 . Percentage : 5% , 50 taka er 10% koto hobe 
+// 7 . sum : 14, 15, 26
+// 8 . Avg : 15, 16, 17
+// 9 . Max/Min : 14, 15, 16
+// 10 . series : 1, 2, 3, 4, 5, 6, 7, 8, 9, 0
+// 11 . Triangle : 
+// 12 . 
